e7f790e8df4767f7c4d1a9498758dd41c34412f2
Toby Zerner <toby.zerner@gmail.com>
2018-04-23 09:33:06 +0930

    Update for webpack

--- js/admin/src/index.js
+++ js/admin/src/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/app';
 
-import FacebookSettingsModal from 'flarum/auth/facebook/components/FacebookSettingsModal';
+import FacebookSettingsModal from './components/FacebookSettingsModal';
 
 app.initializers.add('flarum-auth-facebook', () => {
   app.extensionSettings['flarum-auth-facebook'] = () => app.modal.show(new FacebookSettingsModal());
