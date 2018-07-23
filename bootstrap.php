7362d526421b0dea29bf973b5fe642963e6d6f06
Franz Liedke <franz@develophp.org>
2018-07-23 16:27:21 +0200

    Use new Frontend extender
    See flarum/core#851.

--- bootstrap.php
+++ bootstrap.php
@@ -13,11 +13,11 @@ use Flarum\Auth\Facebook\FacebookAuthController;
 use Flarum\Extend;
 
 return [
-    (new Extend\Assets('forum'))
+    (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->asset(__DIR__.'/less/forum.less'),
+        ->css(__DIR__.'/less/forum.less'),
 
-    (new Extend\Assets('admin'))
+    (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
 
     (new Extend\Routes('forum'))
